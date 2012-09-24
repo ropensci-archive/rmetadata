fc581f440f008c4f90a85da31a18c2c00e2ca597
Scott Chamberlain <myrmecocystus@gmail.com>
2012-09-23 22:46:22 -0700

    added fxn count_identifiers to count number of OAI identifiers a repository holds
    this fxn does quick count by attempting to grab metadata from a call to ListIdentifiers - most do not provide this in the metadata

--- R/count_identifiers.r
+++ R/count_identifiers.r
@@ -1,7 +1,7 @@
 #' Count OAI-PMH identifiers for a data provider.
 #' 
 #' Count identifiers for the data sources from the OAI-PMH list, and others not 
-#' 		on that list, including PMC, DataCite, Hindawi Journals, Dryad, and 
+#'     on that list, including PMC, DataCite, Hindawi Journals, Dryad, and 
 #' 		Pensoft Journals.
 #' 
 #' @import XML httr plyr
@@ -11,41 +11,64 @@
 #' @param fuzzy Do fuzzy search or not (default FALSE). Fuzzy uses agrep.
 #' @author Scott Chamberlain \link{myrmecocystus@@gmail.com}
 #' @examples \dontrun{
-#' # Select a few
-#' count_identifiers(provider = c("datacite","Academic Commons","pensoft","arXiv"))
+#' # Select one
+#' count_identifiers("datacite")
 #' 
-#' # Provide a vector
-#' out <- count_identifiers(provider = providers[1:40,2], useurl = T)
+#' # Select a few
+#' ldply(c("datacite","Academic Commons","pensoft","arXiv"), count_identifiers)
+#' 
+#' # All of them, takes a while, run on AWS AMI if you can
+#' out <- llply(providers[,2], function(x) count_identifiers(x, useurl = T), .inform=T)
+#' outdf <- ldply(out)
+#' str(outdf)
+#' outdf_ <- outdf[!is.na(as.numeric(as.character(outdf$count))), ]  # just those with counts
 #' }
 #' @export
 count_identifiers <- function(provider = NULL, metadataPrefix = 'oai_dc', 
-		fuzzy = FALSE, useurl = FALSE)
+    fuzzy = FALSE, useurl = FALSE, seconds = 3)
 { 
-	if(exists(as.character(substitute(providers)))==TRUE){ NULL } else
-		{ data(providers); message("loaded providers") }
-	
-	args <- compact(list(verb = 'ListIdentifiers', metadataPrefix = metadataPrefix))
-	
-	doit <- function(x, args) {
-		if(useurl == TRUE){ url <- x } else
-		{
-			if(fuzzy){ get_ <- providers[ agrep(x, providers[,1], ...), ] } else
-				{ get_ <- providers[ grep(x, providers[,1]), ] }
-			if(nrow(get_) == 0){ data.frame(x="no match found") } else
-				if(nrow(get_) > 1){  data.frame(repo_name = get_[,1]) } else 
-					{ url <- get_[,"base_url"] }
-		}
-			mm <- GET(url)[[4]]$`content-type`
-			nn <- strsplit(mm, ";")[[1]][[1]]
-			if(nn == "text/html") { "not provided" } else 
-				if(nn == "text/xml") {
-					tt <- try(xmlToList(xmlParse(content(GET(url, query=args), as="text")))$ListIdentifiers$resumptionToken$.attrs[["completeListSize"]])
-					if(class(tt)=="try-error"){ "not provided" } else {
-						if(is.null(tt)==TRUE) { "not provided" } else { tt }
-					}
-			} else { "bad content type" }
-	}	
-	dd <- sapply(provider, function(x) doit(x, args) , USE.NAMES=F)
-	if(useurl){ provider <- providers[providers$base_url %in% provider, 1]} else {provider <- provider}
-	data.frame(provider = provider, count = dd)
+  if(exists(as.character(substitute(providers)))==TRUE){ NULL } else
+  { data(providers); message("loaded providers") }
+  
+  args <- compact(list(verb = 'ListIdentifiers', metadataPrefix = metadataPrefix))
+  
+  if(useurl == TRUE){ url <- provider } else
+  {
+    if(fuzzy){ get_ <- providers[ agrep(provider, providers[,1], ...), ] } else
+    { get_ <- providers[ grep(provider, providers[,1]), ] }
+    if(nrow(get_) == 0){ data.frame(x="no match found") } else
+      if(nrow(get_) > 1){  data.frame(repo_name = get_[,1]) } else 
+      { url <- get_[,"base_url"] }
+  }
+  mm <- tryCatch(GET(url, 
+                     config=timeout(seconds))[[4]]$`content-type`, error = function(e) e$message)
+  if(length(mm)==0) {dd <- "error"} else {
+  if(grepl("timed out", mm)){dd<-"timed out"} else
+  {
+      nn <- strsplit(mm, ";")[[1]][[1]]
+      if(nn == "text/html") { dd<-"not provided" } else 
+        if(nn == "text/xml") {
+          ss <- tryCatch(http_status(GET(url, query=args,  
+                             config=timeout(seconds))), error = function(e) e$message)
+          if(grepl("timed out", ss[[1]])){dd<-"timed out"} else {
+            if(!grepl("success", ss$category)){dd<-"error"} else {
+              tt <- tryCatch(xmlToList(
+                xmlParse(
+                  content(
+                    GET(
+                      url, query=args, config=timeout(seconds)), 
+                    as="text")))$ListIdentifiers$resumptionToken$.attrs[["completeListSize"]],
+                             error = function(e) e$message)
+          if(is.null(tt)){dd<-"not provided"} else
+            if(class(tt)=="try-error"){ dd<-"not provided" } else 
+              if(tt == "subscript out of bounds") { dd<-"not provided" } else
+                  { dd<-tt }
+      }
+      }
+    } else { dd<-"bad content type" }
+  }
+  }
+  if(useurl){ provider <- 
+    providers[providers$base_url %in% provider, 1]} else {provider <- provider}
+  data.frame(provider = provider, count = dd)
 }
