cdb9104e8bcefa39ed06a81211e1ad00dfb295ca
steven2249 <stevenysw@berkeley.edu>
2017-03-22 17:31:53 -0700

    Update test-eu_providers.R

--- tests/testthat/test-eu_providers.R
+++ tests/testthat/test-eu_providers.R
@@ -1,6 +1,9 @@
 context("eu_providers")
 
 test_that("eu_providers", {
-  tt <- eu_providers()
+  tt <- eu_providers(key = Sys.getenv("EUROPEANA_KEY"))
   
+  expect_is(tt, "list")
+  expect_is(tt$meta, "list")
+  expect_is(tt$items, "list")
 })
