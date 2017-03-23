36fb8538d6a78b485ace31a1866f4fe8ee7e24ef
steven2249 <stevenysw@berkeley.edu>
2017-03-22 17:37:04 -0700

    Update test-hathi_bib.R

--- tests/testthat/test-hathi_bib.R
+++ tests/testthat/test-hathi_bib.R
@@ -1,6 +1,17 @@
 context("hathi_bib")
 
 test_that("hathi_bib", {
-  tt <- hathi_bib()
-  
+  tt <- hathi_bib(oclc = 424023)
+
+  expect_is(tt, "list")
+  expect_is(tt$records, "list")
+  expect_is(tt$items, "data.frame")
+  expect_is(tt$items$orig, "character")
+  expect_is(tt$items$fromRecord, "character")
+  expect_is(tt$items$htid, "character")
+  expect_is(tt$items$itemURL, "character")
+  expect_is(tt$items$rightsCode, "character")
+  expect_is(tt$items$lastUpdate, "character")
+  expect_is(tt$items$enumcron, "logical")
+  expect_is(tt$items$usRightsString, "character") 
 })
