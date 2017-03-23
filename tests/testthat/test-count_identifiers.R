16c2a8c0f3a226c6eca5f582c1883f27863a5814
steven2249 <stevenysw@berkeley.edu>
2017-03-22 17:24:21 -0700

    Update test-count_identifiers.R

--- tests/testthat/test-count_identifiers.R
+++ tests/testthat/test-count_identifiers.R
@@ -1,6 +1,9 @@
 context("count_identifiers")
 
 test_that("count_identifiers", {
-  tt <- count_identifiers()
+  tt <- count_identifiers(provider="datacite")
   
+  expect_is(tt, "data.frame")
+  expect_is(tt$provider, "factor")
+  expect_is(tt$count, "factor")
 })
